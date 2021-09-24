bac0730046424b180101f016366e1b81af8534ad
olepunchy <74633934+olepunchy@users.noreply.github.com>
2021-09-24 04:56:09 -0500

    Completed operator overload solution

---
+++ operator_overloads/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+class Complex {
+public:
+  int a, b;
+  void input(string s) {
+    int v1 = 0;
+    int i = 0;
+    while (s[i] != '+') {
+      v1 = v1 * 10 + s[i] - '0';
+      i++;
+    }
+    while (s[i] == ' ' || s[i] == '+' || s[i] == 'i') {
+      i++;
+    }
+    int v2 = 0;
+    while (i < s.length()) {
+      v2 = v2 * 10 + s[i] - '0';
+      i++;
+    }
+    a = v1;
+    b = v2;
+  }
+};
+
+Complex operator+(Complex t, Complex r) {
+  Complex result;
+  result.a = t.a + r.a;
+  result.b = t.b + r.b;
+
+  return result;
+}
+
+ostream &operator<<(ostream &out, Complex x) {
+  out << x.a << "+i" << x.b;
+  return out;
+}
+
+int main(void) {
+  Complex x, y;
+  string s1, s2;
+  cin >> s1;
+  cin >> s2;
+  x.input(s1);
+  y.input(s2);
+  Complex z = x + y;
+  cout << z << endl;
+
+  return 0;
+}
